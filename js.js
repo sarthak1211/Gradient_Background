446f1676fb6b5c2300a82dc5d8085d49d0326d19
sarthaksinghal1211 <32648702+sarthaksinghal1211@users.noreply.github.com>
2020-10-04 14:55:55 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body= document.getElementById("body");
+var h3= document.querySelector("h3");
+
+function colourGradient(){
+    body.style.background="linear-gradient(to right,"+ color1.value+","+color2.value+")";
+    h3.textContent=body.style.background+";";
+}
+color1.addEventListener("input", colourGradient);
+
+color2.addEventListener("input", colourGradient);
